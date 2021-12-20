98667ae201e40c80c9f9f5fa3728a5bafdd3b453
paalso <40a.pvl@gmail.com>
2021-12-20 19:36:43 +0200

    Fix lint-test.yml and test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,11 +5,11 @@ from tests.fixtures.expected import PLAIN_JSONS_DIFF_STR_21
 
 def test_gendiff_plain():
     assert generate_diff(
-        'tests/fixtures/plain1.json',
-        'tests/fixtures/plain2.json'
+        './tests/fixtures/plain1.json',
+        './tests/fixtures/plain2.json'
     ) == PLAIN_JSONS_DIFF_STR_12
 
     assert generate_diff(
-        'tests/fixtures/plain2.json',
-        'tests/fixtures/plain1.json'
+        './tests/fixtures/plain2.json',
+        './tests/fixtures/plain1.json'
     ) == PLAIN_JSONS_DIFF_STR_21
